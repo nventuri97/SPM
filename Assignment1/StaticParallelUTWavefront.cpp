6e0ea17b17c91eef06ea5710084109104077fb50
Nicola Venturi <n.venturi3@studenti.unipi.it>
2024-04-13 12:23:38 +0200

    Final version for Static Parallelization

--- Assignment1/StaticParallelUTWavefront.cpp
+++ Assignment1/StaticParallelUTWavefront.cpp
@@ -25,27 +25,29 @@ void work(std::chrono::microseconds w) {
     while(std::chrono::steady_clock::now() < end);	
 }
 
-void sequentialWavefront(const std::vector<int> &M, const uint64_t &N) {
-	for(uint64_t k = 0; k< N; ++k) {        // for each upper diagonal
-		for(uint64_t i = 0; i< (N-k); ++i) {// for each elem. in the diagonal
-			work(std::chrono::microseconds(M[i*N+(i+k)])); 
-		}
-	}
-}
-
-//Function that compute the wave front in a static parallel way based on a chunk of data
-void staticParallelWavefront(const std::vector<int> &M, const uint64_t &N, const uint64_t &numThreads){
-	//Barrier with which threads wait until the end of a raw
+//Function that compute the wave front in a static parallel way, a thread compute the corresponding element % number of threads
+void staticParallelWavefront(const std::vector<int> &M, const uint64_t &N, const int &numThreads){
+	//Barrier with which threads wait until the end of a row
 	std::barrier greatBarrier(numThreads);
-	//Number of elements that a single thread has to skip to work on the next item
-	const uint64_t chunkSize = N/numThreads;
 	//Threads vector which will contain the threads that compute the elements
-	std::vector<std::thread> threads(numThreads);
+	std::vector<std::thread> threads;
+	threads.reserve(numThreads);
 
 	auto staticParallelization = [&] (uint64_t threadId) -> void {
-		for(uint64_t k = 0; k< N; k+=chunkSize) {        								// for each upper diagonal
-			for(uint64_t i = 0; i<(N-k); ++i) {											// for each elem. in the diagonal
+		//Integer that specifies the starting point for each threads based on its thread ID
+		uint64_t startPoint=threadId;
+
+		for(uint64_t k = 0; k< N; ++k) { 			       								// for each upper diagonal
+			for(uint64_t i = startPoint; i<(N-k); i+=numThreads) {						// for each elem. in the diagonal
 				work(std::chrono::microseconds(M[i*N+(i+k)])); 
+			}
+
+			if(threadId+1 > N-k){
+				greatBarrier.arrive_and_drop();
+				break;
+			}
+			else{
+				greatBarrier.arrive_and_wait();
 			}
 		}
 	};
@@ -66,7 +68,7 @@ int main(int argc, char *argv[]) {
 	int min    = 0;      // default minimum time (in microseconds)
 	int max    = 1000;   // default maximum time (in microseconds)
 	uint64_t N = 512;    // default size of the matrix (NxN)
-	uint64_t numThreads= 4;
+	int numThreads= 4;
 	
 	if (argc != 1 && argc != 2 && argc != 5) {
 		std::printf("use: %s N [min max]\n", argv[0]);
