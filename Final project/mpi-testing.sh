76d5beb936456208e9d7205a73ddc3775bbd95b2
Nicola Venturi <n.venturi3@studenti.unipi.it>
2024-08-28 17:41:24 +0200

    Makefile and SBATCH script for MPI testing

--- Final project/mpi-testing.sh
+++ Final project/mpi-testing.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH -p normal
-#SBATCH -N 4
+#SBATCH -N 1
 #SBATCH --ntasks=8                  # Number of MPI processes (distributed per node)
-#SBATCH --cpus-per-task=1           # Number of CPUs per task (=process)
+#SBATCH --cpus-per-task=1           # Number of CPUs per task (=thread)
 #SBATCH -o ./%j-mpi.log
 #SBATCH -e ./%j-mpi.err
 #SBATCH -t 02:00:00
 
-echo "Test executed on: $SLURM_JOB_NODELIST"
+echo "Test executed on: $SLURM_JOB_NODELIST with $SLURM_NTASKS"
 make broad
-mpirun ./BroadMPIUTWavefront.o $1 $2
+make run_multiple_tests FILE="BroadMPIUTWavefront.o" ARGS="$1 $2"
 echo "done"
